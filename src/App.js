d8e59a406450b670ee600f17b460a15832955ae5
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-05-27 16:15:11 -0700

    react tracked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+import { SharedStateProvider } from './store';
+import Counter from './Counter';
+import TextBox from './TextBox';
+
+const App = () => (
+  <SharedStateProvider>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter />
+        <TextBox />
       </header>
     </div>
-  );
-}
+  </SharedStateProvider>
+);
 
 export default App;
---
+++ src/Counter.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { useSharedState } from './store';
+
+const Counter = () => {
+  const [state, setState] = useSharedState();
+  const increment = () => {
+    setState((prev) => ({ ...prev, count: prev.count + 1 }));
+  };
+  return (
+    <div>
+      {state.count}
+      <button onClick={increment}>+1</button>
+      {Math.random()}
+    </div>
+  );
+};
+
+export default Counter;
